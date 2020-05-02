ac63ec78d7bbad36e9fd9022587f62722edd3b14
Brian Mancini <bmancini@gmail.com>
2020-05-02 13:14:43 -0400

    binary-search: make r inclusive/exclusive algorithms

--- src/binary-search.js
+++ src/binary-search.js
@@ -1,46 +1,83 @@
 module.exports.search = search;
-module.exports.searchr = searchr;
+module.exports.search2 = search2;
+module.exports.searchRecursive = searchRecursive;
+module.exports.searchRecursive2 = searchRecursive2;
 module.exports.lowerBound = lowerBound;
 module.exports.upperBound = upperBound;
 
 /**
- * Finds the index of the target value in the array. Returns -1
- * if the value is not found.
+ * Interative (r exclusive) algorithm that finds the index of the target value
+ * in a sorted array. Returns -1 if the value is not found.
  *
  * @param {number[]} a sorted array
  * @param {number} l left index
- * @param {number} r right index
+ * @param {number} r right index exclusive
  * @param {number} x target value
  * @returns {number} the index of the value in the array
  */
 function search(a, l, r, x) {
 	while (l < r) {
-		let mid = l + Math.floor((r - l) / 2);
+		let mid = Math.floor((r + l) / 2);
 		if (a[mid] === x) return mid;
-		if (a[mid] > x) r = mid;
-		if (a[mid] < x) l = mid + 1;
+		if (x < a[mid]) r = mid;
+		if (x > a[mid]) l = mid + 1;
 	}
 	return -1;
 }
 
 /**
- * Recursively finds the index of the target value in the array. Returns -1
- * if the value is not found.
+ * Interative (r exclusive) algorithm that finds the index of the target value
+ * in a sorted array. Returns -1 if the value is not found.
  *
  * @param {number[]} a sorted array
  * @param {number} l left index
- * @param {number} r right index
+ * @param {number} r right index inclusive
  * @param {number} x target value
  * @returns {number} the index of the value in the array
  */
-function searchr(a, l, r, x) {
-	if (l < r) {
-		let mid = l + Math.floor((r - l) / 2);
+function search2(a, l, r, x) {
+	while (l <= r) {
+		const mid = Math.floor((r + l) / 2);
 		if (a[mid] === x) return mid;
-		if (a[mid] > x) return searchr(a, l, mid, x);
-		if (a[mid] < x) return searchr(a, mid + 1, r, x);
+		if (x < a[mid]) r = mid - 1;
+		if (x > a[mid]) l = mid + 1;
 	}
 	return -1;
+}
+
+/**
+ * Recursively (r exclusive) algoritthm that finds the index of the target value
+ * in the array. Returns -1 if the value is not found.
+ *
+ * @param {number[]} a sorted array
+ * @param {number} l left index
+ * @param {number} r right index exclusive
+ * @param {number} x target value
+ * @returns {number} the index of the value in the array
+ */
+function searchRecursive(a, l, r, x) {
+	if (l < r) {
+		let mid = Math.floor((r + l) / 2);
+		if (a[mid] === x) return mid;
+		if (x < a[mid]) return searchRecursive(a, l, mid, x);
+		if (x > a[mid]) return searchRecursive(a, mid + 1, r, x);
+	}
+	return -1;
+}
+
+/**
+ *
+ * @param {*} a
+ * @param {*} l
+ * @param {*} r
+ * @param {*} x
+ */
+function searchRecursive2(a, l, r, x) {
+	if (l > r) return -1;
+	const mid = Math.floor((r + l) / 2);
+	if (a[mid] === x) return mid;
+	if (x < a[mid]) return searchRecursive2(a, l, mid - 1, x);
+	if (x > a[mid]) return searchRecursive2(a, mid + 1, r, x);
 }
 
 /**
@@ -64,7 +101,7 @@ function lowerBound(a, l, r, x) {
 	if (x < a[0]) return 0;
 
 	while (l < r) {
-		let mid = l + Math.floor((r - l) / 2);
+		let mid = Math.floor((r + l) / 2);
 		if (a[mid] === x) return mid;
 		if (a[mid] < x) l = mid + 1;
 		if (a[mid] > x) r = mid;
@@ -94,7 +131,7 @@ function upperBound(a, l, r, x) {
 
 	// shrink array until we find the value
 	while (l < r) {
-		let mid = l + Math.floor((r - l) / 2);
+		let mid = Math.floor((r + l) / 2);
 		if (a[mid] === x) return mid;
 		if (a[mid] < x) l = mid + 1;
 		if (a[mid] > x) r = mid;
--- test/binary-search.spec.js
+++ test/binary-search.spec.js
@@ -1,11 +1,13 @@
 const { expect } = require('chai');
 const { search } = require('../src/binary-search');
-const { searchr } = require('../src/binary-search');
+const { search2 } = require('../src/binary-search');
+const { searchRecursive } = require('../src/binary-search');
+const { searchRecursive2 } = require('../src/binary-search');
 const { lowerBound } = require('../src/binary-search');
 const { upperBound } = require('../src/binary-search');
 
-describe('binary search', () => {
-	describe('.search iterative', () => {
+describe('binary search - iterative, r exclusive', () => {
+	describe('.search iterative, r exclusive', () => {
 		it('should return -1 when below', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 0;
@@ -55,53 +57,153 @@ describe('binary search', () => {
 		});
 	});
 
-	describe('.search recursive', () => {
+	describe('.search iterative, r inclusive', () => {
 		it('should return -1 when below', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 0;
-			expect(searchr(a, 0, a.length, x)).to.equal(-1);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(-1);
 		});
 
 		it('should return -1 when not found', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 3;
-			expect(searchr(a, 0, a.length, x)).to.equal(-1);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(-1);
 		});
 
 		it('should return first value', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 1;
-			expect(searchr(a, 0, a.length, x)).to.equal(0);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(0);
 		});
 
 		it('should return mid value', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 4;
-			expect(searchr(a, 0, a.length, x)).to.equal(1);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(1);
 		});
 
 		it('should return mid value', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 8;
-			expect(searchr(a, 0, a.length, x)).to.equal(2);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(2);
 		});
 
 		it('should return mid value', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 12;
-			expect(searchr(a, 0, a.length, x)).to.equal(3);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(3);
 		});
 
 		it('should return last value', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 21;
-			expect(searchr(a, 0, a.length, x)).to.equal(4);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(4);
 		});
 
 		it('should return -1 above', () => {
 			let a = [1, 4, 8, 12, 21];
 			let x = 22;
-			expect(searchr(a, 0, a.length, x)).to.equal(-1);
+			expect(search2(a, 0, a.length - 1, x)).to.equal(-1);
+		});
+	});
+
+	describe('binary search - recursive, r exclusive', () => {
+		it('should return -1 when below', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 0;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(-1);
+		});
+
+		it('should return -1 when not found', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 3;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(-1);
+		});
+
+		it('should return first value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 1;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(0);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 4;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(1);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 8;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(2);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 12;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(3);
+		});
+
+		it('should return last value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 21;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(4);
+		});
+
+		it('should return -1 above', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 22;
+			expect(searchRecursive(a, 0, a.length, x)).to.equal(-1);
+		});
+	});
+
+	describe('binary search - recursive, r inclusive', () => {
+		it('should return -1 when below', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 0;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(-1);
+		});
+
+		it('should return -1 when not found', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 3;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(-1);
+		});
+
+		it('should return first value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 1;
+			expect(searchRecursive(a, 0, a.length - 1, x)).to.equal(0);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 4;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(1);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 8;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(2);
+		});
+
+		it('should return mid value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 12;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(3);
+		});
+
+		it('should return last value', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 21;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(4);
+		});
+
+		it('should return -1 above', () => {
+			let a = [1, 4, 8, 12, 21];
+			let x = 22;
+			expect(searchRecursive2(a, 0, a.length - 1, x)).to.equal(-1);
 		});
 	});
 
